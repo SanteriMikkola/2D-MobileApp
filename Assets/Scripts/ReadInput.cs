49a2c16e9647201ee78ff34cc192f86ee562fb39
NickEZz <minime.yt@gmail.com>
2021-04-08 13:01:11 +0300

    Tein laustan layout prototypointia ja visualisointia koodasin niin että kirjoitettu teksti näkyy muistututkset paneelissa

---
+++ Assets/Scripts/ReadInput.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReadInput : MonoBehaviour
+{
+    GameObject MuistutusText;
+    private string input;
+    Text ourComponent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MuistutusText = GameObject.Find("MyText");
+        ourComponent = MuistutusText.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ourComponent.text = input;
+    }
+
+    // Lukee tiedot käyttäjältä
+    public void ReadStringInput(string s){
+        input = s;
+        Debug.Log(input);
+    }
+
+
+
+
+
+}
