fe03657e86fbc4ab1062f1920a8516be1b8bd1b4
SanteriMikkola <santerimikkola04@gmail.com>
2021-05-27 14:16:06 +0300

    Muistutusten koodailua.

--- Assets/Scripts/TimeControl.cs
+++ Assets/Scripts/TimeControl.cs
@@ -97,8 +97,6 @@ public class TimeControl : MonoBehaviour
     public Text dayText;
     public Text clockText;
 
-    public static bool IsThatNextYear;
-
     private const int TIMESCALE = 1;
 
     public static double second, minute, hour, day, month, year;
@@ -292,19 +290,49 @@ public class TimeControl : MonoBehaviour
             CalculatedMin = 0;
         }
 
-        /*if (IsThatNextYear == true)
+        if (YearValue == ConvertedYear && kuukausi < ConvertedMonth)
         {
+            CalculatedMonth = 0;
+        }
+        else if (YearValue == ConvertedYear && kuukausi > ConvertedMonth)
+        {
+            CalculatedMonth = kuukausi - ConvertedMonth;
+        }
 
-        }*/
+        if (kuukausi == ConvertedMonth && PaivaNum < ConvertedDay)
+        {
+            CalculatedDay = 0;
+        }
+        else if (kuukausi == ConvertedMonth && PaivaNum > ConvertedDay)
+        {
+            CalculatedDay = PaivaNum - ConvertedDay;
+        }
 
-        if (CalculatedYear > 0)
+        if (PaivaNum == ConvertedDay && hourNum < ConvertedHour)
+        {
+            CalculatedHour = 0;
+        }
+        else if (PaivaNum == ConvertedDay && hourNum > ConvertedHour)
+        {
+            CalculatedHour = hourNum - ConvertedHour;
+        }
+
+        if (hourNum == ConvertedHour && minuteNum < ConvertedMin)
+        {
+            CalculatedMin = 0;
+        }
+        else if (hourNum == ConvertedHour && minuteNum > ConvertedMin)
+        {
+            CalculatedMin = minuteNum - ConvertedMin;
+        }
+        /*if (CalculatedYear > 0)
         {
             CalculatedMonth = kuukausi - ConvertedMonth;
 
             CalculatedMonth = System.Math.Abs(CalculatedMonth);
 
             Debug.Log(CalculatedMonth);
-        }
+        }*/
 
         TextCallFunction();
 
@@ -1375,88 +1403,51 @@ public class TimeControl : MonoBehaviour
         {
             casevalue = 1;
             YearValue = 2021;
-            IsThatNextYear = false;
         }
         else if (value == 2)
         {
             casevalue = 1;
             YearValue = 2022;
-            if (ConvertedYear == YearValue)
-            {
-                IsThatNextYear = true;
-            }
         }
         else if (value == 3)
         {
             casevalue = 1;
             YearValue = 2023;
-            if (ConvertedYear == YearValue)
-            {
-                IsThatNextYear = true;
-            }
         }
         else if (value == 4)
         {
             casevalue = 1;
             YearValue = 2024;
-            if (ConvertedYear == YearValue)
-            {
-                IsThatNextYear = true;
-            }
         }
         else if (value == 5)
         {
             casevalue = 1;
             YearValue = 2025;
-            if (ConvertedYear == YearValue)
-            {
-                IsThatNextYear = true;
-            }
         }
         else if (value == 6)
         {
             casevalue = 1;
             YearValue = 2026;
-            if (ConvertedYear == YearValue)
-            {
-                IsThatNextYear = true;
-            }
         }
         else if (value == 7)
         {
             casevalue = 1;
             YearValue = 2027;
-            if (ConvertedYear == YearValue)
-            {
-                IsThatNextYear = true;
-            }
         }
         else if (value == 8)
         {
             casevalue = 1;
             YearValue = 2028;
-            if (ConvertedYear == YearValue)
-            {
-                IsThatNextYear = true;
-            }
         }
         else if (value == 9)
         {
             casevalue = 1;
             YearValue = 2029;
-            if (ConvertedYear == YearValue)
-            {
-                IsThatNextYear = true;
-            }
         }
         else if (value == 10)
         {
             casevalue = 1;
             YearValue = 2030;
-            if (ConvertedYear == YearValue)
-            {
-                IsThatNextYear = true;
-            }
         }
         else if (value == 11)
         {
@@ -1554,7 +1545,6 @@ public class TimeControl : MonoBehaviour
                     Minute newMinute = new Minute(minuteNum, Color.white, Minuutit.GetChild(i).gameObject);
                     Minutes.Add(newMinute);
                 }
-                Debug.Log(Minutes.Count);
                 return;
             }
             return;
